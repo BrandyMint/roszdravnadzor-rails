fe3c2444e2d250516c481d4b8cfaee7c2ff71a52
Alexey Illarionov <littlesavage@rambler.ru>
2013-12-17 11:28:55 +0400

    deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :stage, :staging
-set :application, 'stage.HOSTNAME'
-server 'SERVERNAME', user: 'USERNAME', roles: %w{web app db}
+set :application, 'zdrav.icfdev.ru'
+server 'zdrav.icfdev.ru', user: 'wwwzdrav', port: 233, roles: %w{web app db}
 set :rails_env, :staging
 set :branch, proc { ENV['BRANCH'] || `git rev-parse --abbrev-ref HEAD`.chomp }
 fetch(:default_env).merge!(rails_env: :staging)
--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,6 @@
 if defined? Airbrake
   Airbrake.configure do |config|
-    config.api_key = 'API_KEY'
+    config.api_key = '948a0acd7a9cf0169d600d7628450bfe'
     config.host    = 'errbit.brandymint.ru'
     config.port    = 80
     config.secure  = config.port == 443
